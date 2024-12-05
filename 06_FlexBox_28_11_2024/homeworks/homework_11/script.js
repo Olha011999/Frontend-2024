8299f5c31d690ed0856302646b43270bb17da332
Olha Fatina <fowell@urk.net>
2024-12-05 18:00:44 +0100

    Добавлены новые домашние задания и обновлены существующие файлы

---
+++ 06_FlexBox_28_11_2024/homeworks/homework_11/script.js
@@ -0,0 +1,12 @@
+
+let myNumber = 100 / (25 % 3);
+console.log("myNumber:", myNumber); 
+
+let myString = "Hello, " + "world" + "!" + 2024;
+console.log("myString:", myString); 
+
+let isHuman = 42 == "42";
+console.log("isHuman:", isHuman);
+
+let myNumber1 = String("123smth"); 
+console.log("myNumber1:", myNumber1)
